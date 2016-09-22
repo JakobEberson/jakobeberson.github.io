15f31a4a5ab88908a1a64ffc71659d35e969b030
JakobEberson <jakobeberson@gmail.com>
2016-09-22 13:10:09 +0200

    Update startsida.js

--- startsida.js
+++ startsida.js
@@ -3,27 +3,90 @@
 var counter = 0;
 window.setInterval(
 function () {
-counter = counter + 1;
-document.getElementById("counter").innerHTML = counter;
-}, 250);
+counter = counter + 9;
+document.getElementById("counter").innerHTML = counter + " kr";
+}, 500);
 
 var counter2 = 0;
 window.setInterval(
 function () {
-counter2 = counter2 + 3;
-document.getElementById("counter2").innerHTML = counter2;
-}, 125);
+counter2 = counter2 + 12;
+document.getElementById("counter2").innerHTML = counter2 + " kr";
+}, 500);
 
 var counter3 = 0;
 window.setInterval(
 function () {
-counter3 = counter3 + 2;
-document.getElementById("counter3").innerHTML = counter3;
-}, 125);
+counter3 = counter3 + 11;
+document.getElementById("counter3").innerHTML = counter3 + " kr"
+}, 500);
 
 var counter4 = 0;
 window.setInterval(
 function () {
-counter4 = counter4 + 1,5;
-document.getElementById("counter4").innerHTML = counter4;
-}, 125);
+counter4 = counter4 + 10;
+document.getElementById("counter4").innerHTML = counter4 + " kr";
+}, 500);
+
+var counter5 = 0;
+window.setInterval(
+function () {
+counter5 = counter5 + 54;
+document.getElementById("counter5").innerHTML = counter5;
+}, 31,25);
+
+var counter7 = 0;
+window.setInterval(
+function () {
+counter7 = counter7 + 144;
+document.getElementById("counter7").innerHTML = counter7;
+}, 31,25);
+
+var counter6 = 0;
+window.setInterval(
+function () {
+counter6 = counter6 + 4;
+document.getElementById("counter6").innerHTML = counter6;
+}, 31,25);
+
+var counter8 = 0;
+window.setInterval(
+function () {
+counter8 = counter8 + 1281;
+document.getElementById("counter8").innerHTML = counter8;
+}, 31,25);
+
+// Taylor Swift //
+var counter9 = 0;
+window.setInterval(
+function () {
+counter9 = counter9 + 23;
+document.getElementById("counter9").innerHTML = counter9 + " kr";
+}, 500);
+
+// One Direction //
+
+var counter10 = 0;
+window.setInterval(
+function () {
+counter10 = counter10 + 15;
+document.getElementById("counter10").innerHTML = counter10 + " kr";
+}, 500);
+
+// Kevin Hart//
+
+var counter11 = 0;
+window.setInterval(
+function () {
+counter11 = counter11 + 12;
+document.getElementById("counter11").innerHTML = counter11 + " kr";
+}, 500);
+
+// Adele //
+
+var counter12 = 0;
+window.setInterval(
+function () {
+counter12 = counter12 + 11;
+document.getElementById("counter12").innerHTML = counter12 + " kr";
+}, 500);
